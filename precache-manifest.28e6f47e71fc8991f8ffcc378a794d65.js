1dc894f949302ccf961c1b1392edc11ca8be4f79
JrReactDev <tranklove@gmail.com>
2019-11-27 19:56:02 +0000

    Updates

---
+++ precache-manifest.28e6f47e71fc8991f8ffcc378a794d65.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fd14454cabfb3777697f",
+    "url": "/react-portfolio/static/css/main.2af66628.chunk.css"
+  },
+  {
+    "revision": "1e4ad457277730ac33c8",
+    "url": "/react-portfolio/static/js/runtime~main.1e4ad457.js"
+  },
+  {
+    "revision": "373e321f5202e455be8c9cb3018a4d54",
+    "url": "/react-portfolio/static/media/projects.373e321f.jpg"
+  },
+  {
+    "revision": "1ff50151bce324415f1e",
+    "url": "/react-portfolio/static/js/2.1ff50151.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-portfolio/static/media/react.5d5d9eef.svg"
+  },
+  {
+    "revision": "a65c536ad1d9b2b7a1754ff71ae99677",
+    "url": "/react-portfolio/static/media/nodejs.a65c536a.png"
+  },
+  {
+    "revision": "fd14454cabfb3777697f",
+    "url": "/react-portfolio/static/js/main.fd14454c.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a6cd9562dd672282ef5f232167b2c48f",
+    "url": "/react-portfolio/static/media/code.a6cd9562.jpg"
+  },
+  {
+    "revision": "1ff50151bce324415f1e",
+    "url": "/react-portfolio/static/css/2.b552a3b9.chunk.css"
+  },
+  {
+    "revision": "a175ed44e17082b3b630d9ed7fbb627d",
+    "url": "/react-portfolio/index.html"
+  }
+];
